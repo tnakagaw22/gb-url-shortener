31035a01ea1cc10459ed28bdc185c9f0f33abb2f
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2021-02-09 18:06:37 -0500

    added validation logic

--- src/components/UrlInput.js
+++ src/components/UrlInput.js
@@ -5,6 +5,7 @@ import InputGroup from 'react-bootstrap/InputGroup';
 import FormControl from 'react-bootstrap/FormControl';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useForm } from "react-hook-form";
 
 import ErrorSave from './ErrorSave';
 
@@ -14,23 +15,24 @@ import { post } from '../api/baseApi';
 const UrlInput = (props) => {
     const [state, dispatch] = React.useContext(Context);
     const urlInputRef = React.useRef(null);
+    const { register, errors, handleSubmit } = useForm();
+
     const [url, setUrl] = React.useState('');
     const [slug, setSlug] = React.useState('');
     const [saving, setSaving] = React.useState(false);
-    const [errors, setErrors] = React.useState('');
+    const [serverErrors, setServerErrors] = React.useState('');
 
     React.useEffect(() => {
         urlInputRef.current.focus();
     }, [urlInputRef]);
 
-    const handleAddUrl = async (e) => {
-        e.preventDefault();
+    const handleAddUrl = async () => {
 
         setSaving(true);
-        setErrors('');
+        setServerErrors('');
 
         try {
-            let response = await post('links', { url, slug });
+            let response = await post('links', { url: encodeURIComponent(url), slug });
             dispatch({ type: 'ADD_LINK', payload: response.data });
 
             setUrl('');
@@ -38,7 +40,7 @@ const UrlInput = (props) => {
             urlInputRef.current.focus();
         }
         catch (err) {
-            setErrors(err.response.data.errors);
+            setServerErrors(err.response.data.errors);
         }
 
         setSaving(false);
@@ -46,7 +48,7 @@ const UrlInput = (props) => {
 
     return (
         <>
-            <Form onSubmit={handleAddUrl}>
+            <Form onSubmit={handleSubmit(handleAddUrl)}>
                 <Row>
                     <Col xs={12} md={8}>
                         <label htmlFor="url-input">URL</label>
@@ -56,37 +58,58 @@ const UrlInput = (props) => {
                                     http://bely.me/
                                 </InputGroup.Text>
                             </InputGroup.Prepend>
-                            <FormControl id="url-input" aria-describedby="url-prepend"
+                            <FormControl 
+                                id="url-input" aria-describedby="url-prepend"
                                 value={url}
+                                name="urlInput"
                                 onChange={e => setUrl(e.target.value)}
-                                ref={urlInputRef}
+                                ref={(e) => {
+                                    register(e, { required: true });
+                                    urlInputRef.current = e;
+                                }}
+                                isInvalid={!!errors.urlInput}
                                 data-cy="url-input"
-                                required
                             />
+
+                            <Form.Control.Feedback type="invalid">
+                                {errors.urlInput && errors.urlInput.type === "required" && (
+                                    'URL is required'
+                                )}
+                            </Form.Control.Feedback>
                         </InputGroup>
+
                     </Col>
                     <Col xs={6} md={4}>
                         <Form.Group controlId="form-slug">
                             <Form.Label>Slug</Form.Label>
                             <Form.Control
                                 type="text"
+                                name="slugInput"
                                 value={slug}
                                 placeholder="Enter slug"
                                 onChange={e => setSlug(e.target.value)}
+                                ref={register({ maxLength: 100 })}
+                                isInvalid={!!errors.slugInput}
                                 data-cy="slug-input"
                             />
 
+                            <Form.Control.Feedback type="invalid">
+                                {errors.slugInput && errors.slugInput.type === "maxLength" && (
+                                    'Slug needs to be less than 100'
+                                )}
+                            </Form.Control.Feedback>
                         </Form.Group>
                     </Col>
                 </Row>
 
-                <ErrorSave errors={errors} />
+                <ErrorSave errors={serverErrors} />
 
                 <Row className='my-3 justify-content-md-center'>
                     <Col md={4}>
                         <Button
                             block
-                            onClick={!saving ? handleAddUrl : null}
+                            type="submit"
+                            onClick={!saving ? handleSubmit(handleAddUrl) : null}
                             disabled={saving}
                             data-cy="add-link-button"
                         >
